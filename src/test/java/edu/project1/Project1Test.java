6b08b75540f617c96d247147c3fe4dec884aab08
V <slvbtv@gmail.com>
2023-10-22 15:26:54 +0500

    Project1 Initial commit

--- src/test/java/edu/project1/Project1Test.java
+++ src/test/java/edu/project1/Project1Test.java
@@ -1,6 +1,5 @@
 package edu.project1;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,7 +8,7 @@ public class Project1Test {
 
     @Test
     @DisplayName("Wrong input, game is not ending Test")
-    public void consoleHangman_WrongInput_GameIsNotOver_Test(){
+    public void consoleHangman_WrongInput_GameIsNotOver_Test() {
         //Arrange
         String word = Dictionary.getWord();
         String userAnswer = "1";
@@ -21,9 +20,10 @@ public class Project1Test {
         assertThat(Session.gameOver).isFalse();
 
     }
+
     @Test
     @DisplayName("Empty input, game is not ending Test")
-    public void consoleHangman_EmptyInput_GameIsNotOver_Test(){
+    public void consoleHangman_EmptyInput_GameIsNotOver_Test() {
         //Arrange
         String word = Dictionary.getWord();
         String userAnswer = "";
@@ -38,7 +38,7 @@ public class Project1Test {
 
     @Test
     @DisplayName("Wrong guess, attempts < maxAttempts, game is not ending Test")
-    public void session_WrongGuessNotMaxAttempts_GameIsNotOver_Test(){
+    public void session_WrongGuessNotMaxAttempts_GameIsNotOver_Test() {
         //Arrange
         Session.gameOver = false;
         String word = ConsoleHangman.word;
@@ -55,12 +55,12 @@ public class Project1Test {
     }
 
     @Test
-    @DisplayName("Wrong guess, attempts == maxAttempts, game is not ending Test")
-    public void session_WrongGuessMaxAttempts_GameIsOver_Test(){
+    @DisplayName("Wrong guess, attempts >= maxAttempts, game is not ending Test")
+    public void session_WrongGuessMaxAttempts_GameIsOver_Test() {
         //Arrange
         char userAnswer = 'z';
-        ConsoleHangman.maxAttempts = 3;
-        Session.attempts = 2;
+        ConsoleHangman.maxAttempts = 1;
+        Session.attempts = 0;
 
         //Act
         Session.guess(ConsoleHangman.word, userAnswer);
@@ -72,7 +72,7 @@ public class Project1Test {
 
     @Test
     @DisplayName("Correct guess, word is not finished, game is not ending Test")
-    public void session_CorrectGuessWordIsMasked_GameIsNotOver_Test(){
+    public void session_CorrectGuessWordIsMasked_GameIsNotOver_Test() {
         //Arrange
         Session.gameOver = false;
         String word = ConsoleHangman.word;
@@ -90,12 +90,11 @@ public class Project1Test {
 
     @Test
     @DisplayName("Correct guess, word is finished, game is ending Test")
-    public void session_CorrectGuessWordIsUnmasked_GameIsOver_Test(){
+    public void session_CorrectGuessWordIsUnmasked_GameIsOver_Test() {
         //Arrange
         String word = ConsoleHangman.word;
         char letter = 'a';
         ConsoleHangman.maskedWord = word;
-
 
         //Act
         Session.guess(word, letter);
