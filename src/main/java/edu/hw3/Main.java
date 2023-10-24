31ac49e56f43f4b9cd94e84e5cb10f8562ee2bda
V <slvbtv@gmail.com>
2023-10-24 15:20:44 +0500

    Upload Task5 problem to review

---
+++ src/main/java/edu/hw3/Main.java
@@ -0,0 +1,10 @@
+package edu.hw3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Task5.parseContacts(new String[]{"Paul Ardos", "Leonhard Culer", "Carl Bauss"}, "ASC"));
+    }
+}
---
+++ src/main/java/edu/hw3/Task5.java
@@ -0,0 +1,25 @@
+package edu.hw3;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Task5 {
+    public static List<String> parseContacts(String[] names, String sortType) {
+        List<String> list = Arrays.asList(names);
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] nameSurnameOne = o1.split("\\s");
+                String[] nameSurnameTwo = o2.split("\\s");
+                if (sortType.equals("ASC")) {
+                    return nameSurnameOne[1].compareTo(nameSurnameTwo[1]);
+                }
+                return -(nameSurnameOne[1].compareTo(nameSurnameTwo[1]));
+            }
+        });
+        return list;
+
+    }
+}
