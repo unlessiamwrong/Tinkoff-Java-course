d878dd3f7e4f9e46d77bc6dabaa82b5d6ea8ca36
V <slvbtv@gmail.com>
2023-11-13 19:21:56 +0500

    fix: add @TempDir annotation

--- src/test/java/edu/hw6/Task3Test.java
+++ src/test/java/edu/hw6/Task3Test.java
@@ -2,24 +2,24 @@ package edu.hw6;
 
 import edu.hw6.Task3.Executor;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task3Test {
 
+    @TempDir
+    File tempDir;
+
     @Test
-    void abstractFilter_Test() throws IOException {
-        //Arrange
-        Path path = Path.of("C:\\Users\\Вячеслав\\Tinkoff-Java-Course\\Tinkoff-Java-Course");
+    void abstractFilter_Test(@TempDir Path tempDir) throws IOException {
+        DirectoryStream<Path> result = Executor.execute(tempDir);
 
-        //Act
-        DirectoryStream<Path> result = Executor.execute(path);
-
-        //Assert
         assertThat(result).isNotNull();
     }
 }
