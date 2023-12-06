251fd28d5ddfab9c4229050779a5a7e507c6959e
V <slvbtv@gmail.com>
2023-12-06 15:19:18 +0500

    third commit: Hw9. Delete testDir.

---
+++ src/test/java/edu/hw9/Task2Test.java
@@ -0,0 +1,40 @@
+package edu.hw9;
+
+import edu.hw9.Task2.DirSearch;
+import edu.hw9.Task2.FileSearch;
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task2Test {
+
+    @Test void dirSearch_ReturnsCorrect_Test() {
+        //Arrange
+        ForkJoinPool pool = new ForkJoinPool();
+        File root = new File("src/main");
+
+        //Act
+        List<File> result = pool.invoke(new DirSearch(root));
+
+        //Assert
+        assertThat(result.size()).isEqualTo(0);
+
+    }
+
+    @Test void fileSearch_ReturnsCorrect_Test() {
+        //Arrange
+        ForkJoinPool pool = new ForkJoinPool();
+        File root = new File("src/main/resources");
+        long size = 3000;
+        String extension = ".txt";
+
+        //Act
+        List<File> result = pool.invoke(new FileSearch(root, size, extension));
+
+        //Assert
+        assertThat(result.size()).isEqualTo(1);
+
+    }
+}
